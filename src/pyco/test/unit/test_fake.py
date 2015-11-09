167eaeb3b9727de20d4102c4c41f140ed750a97b
Attilio Donà <attilio.dona@gmail.com>
2015-11-09 11:46:29 +0100

    unit test

--- src/pyco/test/unit/test_fake.py
+++ src/pyco/test/unit/test_fake.py
@@ -4,33 +4,46 @@ Created on Mar 21, 2011
 @author: adona
 '''
 import unittest #@UnresolvedImport
-from pyco.device import device, ConnectionRefused
+from pyco.device import device, ConnectionRefused, PermissionDenied
 from os import environ
 
 from pyco import log
+
+
+TELNET_PORT = 7777
 
 # create logger
 log = log.getLogger("test")
 
 class Test(unittest.TestCase):
     
-    @unittest.skip("skipping")
+    #@unittest.skip("skipping")
     def testFakeOk(self):
-        print("should not work")
-        log.info("testFakeOk ...")
-        h = device('telnet://%s:%s@%s:7777' % ('username', 'secret', 'localhost'))
+        log.debug("testFakeOk ...")
+        h = device('telnet://%s:%s@%s:%d' % 
+                   ('username', 'secret', 'localhost',TELNET_PORT))
+        
         h.maxWait = 2
         out = h('id')
-        self.assertRegexpMatches(out, 'uid=[0-9]+\(pyco\).*')
+        self.assertRegex(out, 'uid=[0-9]+\(pyco\).*')
 
+    @unittest.skip("skipping")
+    def testWrongPassword(self):
+        log.debug("testWrongPassword ...")
+        h = device('telnet://%s:%s@%s:%d' % 
+                   ('username', 'wrong_pwd', 'localhost', TELNET_PORT))
+        
+        self.assertRaises(PermissionDenied, h, 'id')
+
+    @unittest.skip("skipping")
     def testNativeTelnet(self):
         log.info("testNative ...")
         user = environ['USER']
         log.info("USER: [%s]" % user)
         h = device('telnet://%s:%s@%s' % (user, user, 'localhost'))
         h.maxWait = 10
-        out = h('id')
-        log.info(out)
+        
+        self.assertRaises(PermissionDenied, h, 'id')
  
  
 
